2a489f557b926e5f43721909bcd69f39cdae6934
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-06-03 17:10:09 +0300

    adding routing

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import AboutHero from "../components/about/AboutHero"
+
+const About = () => {
+  return (
+    <div className="min-h-screen">
+      <AboutHero />
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import Hero from "../components/home/Hero"
+
+const Home = () => {
+  return (
+    <div className="pt-32 min-h-screen items-start justify-start flex flex-col">
+      <Hero />
+    </div>
+  )
+}
+
+export default Home
