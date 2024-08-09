dfec059225edce6089df98a763b93e0e64dc5d5b
NathnaelTB <natibekaffa12@gmail.com>
2024-08-09 13:00:13 +0300

    BlogPage: BlogIntro section added

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,9 +1,11 @@
 import BlogHero from "../components/blogs/BlogHero";
+import BlogIntro from "../components/blogs/BlogIntro";
 
 const Blog = () => {
   return (
     <div className="w-screen min-h-screen">
       <BlogHero />
+      <BlogIntro />
     </div>
   );
 };
