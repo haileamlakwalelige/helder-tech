96557f977122b8fe1d9e6b91421c0ef5ba5aefb4
NathnaelTB <natibekaffa12@gmail.com>
2024-08-12 11:07:12 +0300

    BlogMenu: buttons made dynamic

--- src/components/blogs/BlogMenu.tsx
+++ src/components/blogs/BlogMenu.tsx
@@ -1,14 +1,54 @@
+import { useState } from "react";
+
 const BlogMenu = () => {
-    return (
-        <div className="w-full overflow-x-hidden flex flex-col">
-            <div className="relative left-[5%]">
-                <button className="w-[120px] h-[60px] bg-white text-titleFont">Insights</button>
-                <button className="w-[120px] h-[60px] bg-titleFont text-white">Industry</button>
-                <button className="w-[120px] h-[60px] bg-titleFont text-white">News</button>
-            </div>
-            <div className="w-full h-[3px] bg-titleFont"></div>
-        </div>
-    );
-}
+  const inactiveColor = "bg-titleFont text-white";
+  const activeColor = "bg-white text-titleFont";
+
+  const [insightsStyle, setInsightsStyle] = useState(activeColor);
+  const [industryStyle, setIndustryStyle] = useState(inactiveColor);
+  const [newsStyle, setNewsStyle] = useState(inactiveColor);
+
+  const insightsHandler = () => {
+    setInsightsStyle(activeColor);
+    setIndustryStyle(inactiveColor);
+    setNewsStyle(inactiveColor);
+  };
+  const industryHandler = () => {
+    setIndustryStyle(activeColor);
+    setInsightsStyle(inactiveColor);
+    setNewsStyle(inactiveColor);
+  };
+  const newsHandler = () => {
+    setNewsStyle(activeColor);
+    setInsightsStyle(inactiveColor);
+    setIndustryStyle(inactiveColor);
+  };
+
+  return (
+    <div className="w-full overflow-x-hidden flex flex-col">
+      <div className="relative left-[5%]">
+        <button
+          className={`w-[120px] h-[60px] transition-colors duration-300 ${insightsStyle}`}
+          onClick={insightsHandler}
+        >
+          Insights
+        </button>
+        <button
+          className={`w-[120px] h-[60px] transition-colors duration-300 ${industryStyle}`}
+          onClick={industryHandler}
+        >
+          Industry
+        </button>
+        <button
+          className={`w-[120px] h-[60px] transition-colors duration-300 ${newsStyle}`}
+          onClick={newsHandler}
+        >
+          News
+        </button>
+      </div>
+      <div className="w-full h-[3px] bg-titleFont"></div>
+    </div>
+  );
+};
 
 export default BlogMenu;
